c97ec15de3658f00e8c34408a88d47442699ff36
brojor <bronislav.jordan@gmail.com>
2024-07-16 19:51:17 +0200

    feat: add plan selection form

---
+++ components/AToggle.vue
@@ -0,0 +1,77 @@
+<template>
+	<label class="switch" tabindex="0" @keypress="handleKeypress">
+		<input v-model="isOn" type="checkbox" tabindex="-1">
+		<span class="slider"></span>
+	</label>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['change'])
+const isOn = ref(false)
+
+watch(isOn, () => {
+	emit('change', isOn.value)
+})
+
+const handleKeypress = (event: KeyboardEvent) => {
+	if (event.key === 'Enter' || event.key === ' ') {
+		isOn.value = !isOn.value
+	}
+}
+
+</script>
+
+<style scoped>
+.switch {
+	position: relative;
+	display: inline-block;
+	width: 39px;
+	height: 22px;
+}
+
+.switch input {
+	opacity: 0;
+	width: 0;
+	height: 0;
+}
+
+.slider {
+	position: absolute;
+	cursor: pointer;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: var(--clr-marine-blue);
+	-webkit-transition: .4s;
+	transition: .4s;
+	border-radius: 34px;
+}
+
+.slider:before {
+	position: absolute;
+	content: "";
+	height: 14px;
+	width: 14px;
+	left: 4px;
+	bottom: 4px;
+	background-color: white;
+	-webkit-transition: .4s;
+	transition: .4s;
+	border-radius: 50%;
+}
+
+input:checked+.slider {
+	background-color: var(--clr-marine-blue);
+}
+
+input:focus+.slider {
+	box-shadow: 0 0 1px var(--clr-marine-blue);
+}
+
+input:checked+.slider:before {
+	-webkit-transform: translateX(16px);
+	-ms-transform: translateX(16px);
+	transform: translateX(16px);
+}
+</style>
--- components/PlanSelection.vue
+++ components/PlanSelection.vue
@@ -2,18 +2,122 @@
 	<div class="content-inner">
 		<h1>Select your plan</h1>
 		<p class="sub-heading">You have the option of monthly or yearly billing.</p>
+
+		<div class="plan-cards">
+			<div class="plan-card" v-for="(plan, i) in plans" :key="plan.name" :class="{ active: i === activeIndex }"
+				@click="activeIndex = i" tabindex="0" @keypress="handleKeypress(i, $event)">
+				<component :is="plan.icon" />
+				<h2>{{ plan.name }}</h2>
+				<p>{{ isYearly ? `$${plan.yearlyPrice}/yr` : `$${plan.monthlyPrice}/mo` }}</p>
+				<p v-show="isYearly">2 months free</p>
+			</div>
+		</div>
+
+		<div class="payment-method">
+			<span :class="{ active: !isYearly }">Monthly</span>
+			<AToggle @change="handleToggleChange" />
+			<span :class="{ active: isYearly }">Yearly</span>
+		</div>
+
 	</div>
 	<NavButtons :step-num="stepNum" @next-step="$emit('next-step')" @previous-step="$emit('previous-step')" />
 </template>
 
 
 <script setup lang="ts">
+import IconArcade from '~/components/Icon/Arcade.vue'
+import IconAdvanced from '~/components/Icon/Advanced.vue'
+import IconPro from '~/components/Icon/Pro.vue'
+
+
 defineEmits(['next-step', 'previous-step'])
 defineProps<{
 	stepNum: number
 }>()
 
+const activeIndex = ref(0)
+const isYearly = ref(false)
+
+const plans = [
+	{ name: 'Arcade', monthlyPrice: 9, yearlyPrice: 90, icon: IconArcade },
+	{ name: 'Advanced', monthlyPrice: 12, yearlyPrice: 120, icon: IconAdvanced },
+	{ name: 'Pro', monthlyPrice: 15, yearlyPrice: 150, icon: IconPro },
+]
+
+const handleToggleChange = (isOn: boolean) => {
+	isYearly.value = isOn
+}
+
+const handleKeypress = (index: number, event: KeyboardEvent) => {
+	if (event.key === 'Enter' || event.key === ' ') {
+		activeIndex.value = index
+	}
+}
 </script>
 
 
-<style scoped></style>
+<style scoped>
+.plan-cards {
+	display: grid;
+	gap: 1rem;
+	grid-auto-flow: column;
+	grid-auto-columns: 1fr;
+	margin-top: 2.5rem;
+}
+
+.plan-card {
+	display: flex;
+	flex-direction: column;
+	padding: 1rem;
+	border-radius: 0.5rem;
+	border: 1px solid var(--clr-light-gray);
+	cursor: pointer;
+}
+
+.plan-card:hover,
+.plan-card:focus {
+	border-color: var(--clr-purplish-blue);
+}
+
+.plan-card.active {
+	border-color: var(--clr-purplish-blue);
+	background-color: var(--clr-magnolia);
+}
+
+.plan-card h2 {
+	font-size: 1rem;
+	margin-top: 2.5rem;
+}
+
+.plan-card p:first-of-type {
+	font-size: 0.875rem;
+	color: var(--clr-cool-gray);
+}
+
+.plan-card p:last-of-type {
+	margin-top: 0.25rem;
+	font-size: 0.75rem;
+}
+
+.payment-method {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 1.5rem;
+	margin-top: 2rem;
+	background-color: var(--clr-magnolia);
+	color: var(--clr-cool-gray);
+	padding-block: 0.75rem;
+	border-radius: 0.5rem;
+	font-size: 0.875rem;
+	font-weight: 500;
+}
+
+.payment-method span {
+	transition: color 0.4s;
+}
+
+.payment-method span.active {
+	color: var(--clr-marine-blue);
+}
+</style>
